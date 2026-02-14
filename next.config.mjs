375a5b5fa7a10a9942e06f5a65400f399839edcb
Freedom9100 <diskor07@gmail.com>
2026-02-14 21:32:01 +0300

    Configure GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  basePath: "/14-february-card",
+  assetPrefix: "/14-february-card/",
   typescript: {
     ignoreBuildErrors: true,
-  }
+  },
 }
 
 export default nextConfig
